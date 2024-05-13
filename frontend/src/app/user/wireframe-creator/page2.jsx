b5880658071dbad738040e042a75a0ed64b194a1
m-triple-m <triplem656@gmail.com>
2024-05-13 14:36:03 +0530

    ..

---
+++ frontend/src/app/user/wireframe-creator/page2.jsx
@@ -0,0 +1,74 @@
+'use client';
+import React, { useCallback } from 'react';
+import ReactFlow, {
+    useNodesState, useEdgesState, addEdge, MiniMap,
+    Controls,
+    Background,
+    Handle,
+    Position,
+} from 'reactflow';
+
+import 'reactflow/dist/style.css';
+import DND from '../dnd';
+
+const initialNodes = [
+    { id: '1', position: { x: 0, y: 0 }, data: { label: '1' }, dragHandle: '.custom-drag-handle', type: "BlockNode", isConnectable: true },
+    // { id: '2', position: { x: 0, y: 100 }, data: { label: '2' }, dragHandle: '.custom-drag-handle', type: "BlockNode", isConnectable: true },
+];
+const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
+
+const nodeTypes = {
+    BlockNode: ({ isConnectable, id }) => {
+        // console.log(data);
+        return (
+            <div width={800} className='shadow-lg'>
+                <div className='w-full bg-white p-5 rounded-lg border-2 border-red-700'>
+                    <h2 className='custom-drag-handle'>Wireframe Block</h2>
+                    <DND />
+                </div>
+                <Handle
+                    type="target"
+                    position={Position.Top}
+                    id={id + "handle1"}
+                    style={{ top: 0, background: "#555" }}
+                    isConnectable={isConnectable}
+                />
+                <Handle
+                    type="source"
+                    position={Position.Bottom}
+                    id={id + "handle2"}
+                    style={{ bottom: 0, background: "#555" }}
+                    isConnectable={isConnectable}
+                />
+            </div>
+        );
+    },
+};
+
+export default function App() {
+    const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+    const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+    const onConnect = useCallback(
+        (params) => setEdges((eds) => addEdge(params, eds)),
+        [setEdges],
+    );
+
+    return (
+
+        <div style={{ width: '100vw', height: '100vh', backgroundColor: '#888' }}>
+            <ReactFlow
+                nodes={nodes}
+                edges={edges}
+                onNodesChange={onNodesChange}
+                onEdgesChange={onEdgesChange}
+                onConnect={onConnect}
+                nodeTypes={nodeTypes}
+            >
+                <Controls />
+                <MiniMap />
+                <Background variant="dots" gap={12} size={1} />
+            </ReactFlow>
+        </div>
+    );
+}
